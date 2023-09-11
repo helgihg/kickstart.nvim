791923550b6d755a37009af23d4024570c171c98
Helgi Hrafn Gunnarsson <helgi@binary.is>
2023-09-11 15:42:46 +0000

    Telescope setting set to vertical.

--- init.lua
+++ init.lua
@@ -305,6 +305,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
+    layout_strategy = 'vertical',
+    layout_config = { height = 0.95, width = 0.95 },
     mappings = {
       i = {
         ['<C-u>'] = false,
