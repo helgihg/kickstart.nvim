9a2cae2eda672eb953971221e9525c6d8fac6688
Helgi Hrafn Gunnarsson <helgi@binary.is>
2024-01-25 21:41:28 +0000

    indent-blankline disabled.

--- init.lua
+++ init.lua
@@ -199,6 +199,7 @@ require('lazy').setup({
     }
   },
 
+  --[[
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
@@ -209,6 +210,7 @@ require('lazy').setup({
       show_trailing_blankline_indent = false,
     },
   },
+  --]]
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
