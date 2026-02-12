518e338eee7b5663340386b9ad2dc080407910f4
Helgi Hrafn Gunnarsson <helgi@binary.is>
2026-02-12 13:40:17 +0000

    Keybindings for tab manipulation.

---
+++ lua/custom/plugins/keybindings.lua
@@ -0,0 +1,5 @@
+vim.keymap.set('n', '<Tab>', '<Cmd>tabnext<CR>', { silent = true })
+vim.keymap.set('n', '<S-Tab>', '<Cmd>tabprevious<CR>', { silent = true })
+vim.keymap.set('n', 't', '<Cmd>tabnew<CR>', { silent = true })
+
+return {}
