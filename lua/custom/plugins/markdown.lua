572880b1b1b1be16c16a5006ab84a25d17fcadde
Helgi Hrafn Gunnarsson <helgi@binary.is>
2026-05-15 18:14:33 +0000

    Markdown: ToC opens to the left, not the right.

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -4,17 +4,20 @@ return {
     vim.g.vim_markdown_folding_disabled = 1
     vim.g.vim_markdown_toc_autofit = 1
 
-    vim.api.nvim_create_user_command('MdTocLeft', function()
-      local prev_win = vim.api.nvim_get_current_win()
-
-      vim.cmd 'Toc'
-      local toc_win = vim.api.nvim_get_current_win()
-
-      vim.cmd 'wincmd H'
-      vim.api.nvim_set_option_value('number', false, { win = toc_win })
-      vim.api.nvim_set_option_value('relativenumber', false, { win = toc_win })
-
-      vim.api.nvim_set_current_win(prev_win)
-    end, {})
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'markdown',
+      callback = function(ev)
+        pcall(vim.api.nvim_buf_del_user_command, ev.buf, 'Toc')
+        vim.api.nvim_buf_create_user_command(ev.buf, 'Toc', function()
+          local prev_win = vim.api.nvim_get_current_win()
+          vim.cmd 'Tocv'
+          vim.cmd 'wincmd H'
+          local toc_win = vim.api.nvim_get_current_win()
+          vim.api.nvim_set_option_value('number', false, { win = toc_win })
+          vim.api.nvim_set_option_value('relativenumber', false, { win = toc_win })
+          vim.api.nvim_set_current_win(prev_win)
+        end, {})
+      end,
+    })
   end,
 }
