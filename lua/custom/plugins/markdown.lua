16ce3884076b558ebc4d43586296e2ad228f949c
Helgi Hrafn Gunnarsson <helgi@binary.is>
2026-05-15 18:12:36 +0000

    Markdown: Fix to some crap.

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -4,19 +4,15 @@ return {
     vim.g.vim_markdown_folding_disabled = 1
     vim.g.vim_markdown_toc_autofit = 1
 
-    vim.api.nvim_create_user_command('TocLeft', function()
+    vim.api.nvim_create_user_command('MdTocLeft', function()
       local prev_win = vim.api.nvim_get_current_win()
-
-      local s = vim.o.splitright
-      vim.o.splitright = false
 
       vim.cmd 'Toc'
       local toc_win = vim.api.nvim_get_current_win()
 
+      vim.cmd 'wincmd H'
       vim.api.nvim_set_option_value('number', false, { win = toc_win })
       vim.api.nvim_set_option_value('relativenumber', false, { win = toc_win })
-
-      vim.o.splitright = s
 
       vim.api.nvim_set_current_win(prev_win)
     end, {})
