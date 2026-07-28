957563fa5e060cd901bcc42bcaa2fbadf5f7ccca
Helgi Hrafn Gunnarsson <helgi@binary.is>
2026-07-28 18:11:22 +0000

    Apparently improved Claude Code setup.

--- lua/custom/plugins/claude.lua
+++ lua/custom/plugins/claude.lua
@@ -1,6 +1,19 @@
 return {
-  packs = { 'https://github.com/greggh/claude-code.nvim' },
+  packs = {
+    'https://github.com/folke/snacks.nvim',
+    'https://github.com/coder/claudecode.nvim',
+  },
   setup = function()
-    require('claude-code').setup()
+    require('claudecode').setup()
+
+    vim.keymap.set('n', '<leader>ac', '<cmd>ClaudeCode<cr>', { desc = 'Toggle Claude' })
+    vim.keymap.set('n', '<leader>af', '<cmd>ClaudeCodeFocus<cr>', { desc = 'Focus Claude' })
+    vim.keymap.set('n', '<leader>ar', '<cmd>ClaudeCode --resume<cr>', { desc = 'Resume Claude' })
+    vim.keymap.set('n', '<leader>aC', '<cmd>ClaudeCode --continue<cr>', { desc = 'Continue Claude' })
+    vim.keymap.set('n', '<leader>am', '<cmd>ClaudeCodeSelectModel<cr>', { desc = 'Select Claude model' })
+    vim.keymap.set('n', '<leader>ab', '<cmd>ClaudeCodeAdd %<cr>', { desc = 'Add current buffer' })
+    vim.keymap.set('v', '<leader>as', '<cmd>ClaudeCodeSend<cr>', { desc = 'Send to Claude' })
+    vim.keymap.set('n', '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>', { desc = 'Accept diff' })
+    vim.keymap.set('n', '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', { desc = 'Deny diff' })
   end,
 }
